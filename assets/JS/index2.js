f82863230c8e768fe27e22da9ecf635552bc533f
Oleksandra <oleksandrashkutova@gmail.com>
2025-09-09 12:38:22 +0300

    Add events.

--- assets/JS/index2.js
+++ assets/JS/index2.js
@@ -1,17 +1,44 @@
+
 // керування подіями
 
 const body = document.body;
 const section = document.querySelector('section');
 const button = document.querySelector('button');
-
+/*
 function btnClickHandler (e){
     console.log('this:', this);
     console.log('e.target:', e.target);
     console.log('e.currentTarget:', e.currentTarget);
+console.log('--------------------------------------------');
 
-    this.style.backgroundColor = 'red';
-
+    //this.style.backgroundColor = 'red'
+    e.target.style.backgroundColor = 'red';
 };
 
+// порядок виконання на сплиття button=>section=>body
 button.addEventListener('click', btnClickHandler);
+section.addEventListener('click', btnClickHandler);
+body.addEventListener('click', btnClickHandler);
+
+//порядок виконання body(на зануренні)=> button=> section
+button.addEventListener('click', btnClickHandler);
+section.addEventListener('click', btnClickHandler);
+body.addEventListener('click', btnClickHandler, {capture: true});
+*/
+
+//припинення розповсюдження подій
+
+let clickCounter = 0;
+body.addEventListener('click', () => {
+    clickCounter++;
+    console.log(clickCounter);
+});
+
+button.addEventListener('click', e => {
+    e.stopPropagation();
+    //зупинити розповсюдження події на сплиття на кнопці
+});
+
+//тобто, при нажиманні кнопки дія не буде виконуватись на секції та баді, 
+//бо зупиниться на самій кнопці
 
